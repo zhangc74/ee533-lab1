c20c1af100c61ce98edb32088754896ac1611196
zhangc74 <zhangc74@users.noreply.github.com>
2026-01-17 13:59:58 -0800

    Day 3: implemented concurrent TCP server with fork

--- server.c
+++ server.c
@@ -1,9 +1,12 @@
-/* A simple server in the internet domain using TCP
-   The port number is passed as an argument */
+/* A concurrent TCP server */
 #include <stdio.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <strings.h>
+
 
 void error(char *msg)
 {
@@ -11,37 +14,76 @@ void error(char *msg)
     exit(1);
 }
 
+void dostuff(int sockfd)
+{
+    char buffer[256];
+    int n;
+
+    bzero(buffer,256);
+    n = read(sockfd, buffer, 255);
+    if (n < 0) error("ERROR reading from socket");
+
+    printf("Here is the message: %s\n", buffer);
+
+    n = write(sockfd, "I got your message", 18);
+    if (n < 0) error("ERROR writing to socket");
+}
+
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno, clilen;
-     char buffer[256];
-     struct sockaddr_in serv_addr, cli_addr;
-     int n;
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
+    int sockfd, newsockfd, portno;
+    socklen_t clilen;
+    struct sockaddr_in serv_addr, cli_addr;
+    pid_t pid;
+
+    if (argc < 2) {
+        fprintf(stderr, "ERROR, no port provided\n");
+        exit(1);
+    }
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
         error("ERROR opening socket");
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
-              error("ERROR on binding");
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-     if (newsockfd < 0) 
-          error("ERROR on accept");
-     bzero(buffer,256);
-     n = read(newsockfd,buffer,255);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
-     n = write(newsockfd,"I got your message",18);
-     if (n < 0) error("ERROR writing to socket");
-     return 0; 
+
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    portno = atoi(argv[1]);
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+
+    if (bind(sockfd, (struct sockaddr *) &serv_addr,
+             sizeof(serv_addr)) < 0)
+        error("ERROR on binding");
+
+    listen(sockfd, 5);
+    clilen = sizeof(cli_addr);
+
+
+    while (1) {
+        newsockfd = accept(sockfd,
+            (struct sockaddr *) &cli_addr,
+            &clilen);
+
+        if (newsockfd < 0)
+            error("ERROR on accept");
+
+        pid = fork();
+
+        if (pid < 0)
+            error("ERROR on fork");
+
+        if (pid == 0) {   // 子进程
+            close(sockfd);        // 子进程不用监听 socket
+            dostuff(newsockfd);  // 和 client 通信
+            close(newsockfd);
+            exit(0);
+        }
+        else {            // 父进程
+            close(newsockfd);    // 父进程不用通信 socket
+        }
+    }
+
+
+    return 0;
 }
